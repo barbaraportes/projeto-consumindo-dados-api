c2e74ec217514ddcb53bf5fc9512d105e19e301d
Barbara Portes <134977381+barbaraportes@users.noreply.github.com>
2023-09-13 12:12:39 -0300

    delete metodos then evitando callback hell

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json') 
-.then(resposta => resposta.json()) //metodo then quando a promise foi resolvida - acessa o valor retornado do fetch
-.then(r => {
-    if (r.erro) {
-        throw Error('Esse CEP não existe!')
-    } else {
-        console.log(r)
-    }  
-    
-    })
-.catch(erro => console.log(erro)) //metodo catch quando a promise foi rejeitada
-.finally(mensagem => console.log('Processamento concluído!')); //metodo finally é exibido independente se foi resolvido ou rejeitado
+var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json')
 
-//quando tem varios metodos then é chamado de callback hell-não é bom
+
+
 
 console.log(consultaCEP);
 
