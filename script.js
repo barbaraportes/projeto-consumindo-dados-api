bcac686b9c0bffb5b7942e761e14dc40e3fb7d30
Barbara Portes <134977381+barbaraportes@users.noreply.github.com>
2023-09-13 12:11:44 -0300

    Update script.js
    varios then = varios callback
    situação chamada callback hell > não é bom

--- script.js
+++ script.js
@@ -11,5 +11,7 @@ var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json')
 .catch(erro => console.log(erro)) //metodo catch quando a promise foi rejeitada
 .finally(mensagem => console.log('Processamento concluído!')); //metodo finally é exibido independente se foi resolvido ou rejeitado
 
+//quando tem varios metodos then é chamado de callback hell-não é bom
+
 console.log(consultaCEP);
 
