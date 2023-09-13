da3ce612dc0b165bb860ec972ae8f37d3d07fc83
Barbara Portes <134977381+barbaraportes@users.noreply.github.com>
2023-09-13 12:08:00 -0300

    metodo catch  e finally

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json')
-.then(resposta => resposta.json())
-.then(r => console.log(r));
+var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json') 
+.then(resposta => resposta.json()) //metodo then quando a promise foi resolvida - acessa o valor retornado do fetch
+.then(r => {
+    if (r.erro) {
+        throw Error('Esse CEP não existe!')
+    } else {
+        console.log(r)
+    }  
+    
+    })
+.catch(erro => console.log(erro)) //metodo catch quando a promise foi rejeitada
+.finally(mensagem => console.log('Processamento concluído!')); //metodo finally é exibido independente se foi resolvido ou rejeitado
 
 console.log(consultaCEP);
 
