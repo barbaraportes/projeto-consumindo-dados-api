42f9073fcbb39386ae74fc4c7de85fa4e8931114
Barbara Portes <134977381+barbaraportes@users.noreply.github.com>
2023-09-13 12:17:42 -0300

    async await

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json')
+async function buscaEndereco () {
 
+    var consultaCEP = await fetch('https://viacep.com.br/ws/01001000/json');
+    var consultaCEPConvertida = await consultaCEP.json();
+    console.log(consultaCEPConvertida);
+}
 
+buscaEndereco();
 
-
-console.log(consultaCEP);
 
